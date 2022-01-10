c0bc2f905d95b3715bf255663ceb4bfd51d7ca8c
Alexandra Holden <alexandraholden@Alexandras-MacBook-Air.local>
2022-01-10 18:50:37 -0500

    created js event listener for form element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,20 @@
-// Assign JavaScript variable to items with the id "taskAdder"
-var addTaskButton = document.querySelector("#taskAdder");
+// Assign JavaScript variable to the entire form
+var formEl = document.querySelector("#task-form");
 
-// Assign JavaScript variable to items with the id "tasksToDo"
+// Assign JavaScript variable to the parent ul element
 var tasksToDoEl = document.querySelector("#tasksToDo");
 
 // Create a function that appends child li elements to the parent ul elemnt
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
     // Initiate first callback function
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
-    // Initiate second callback function
     tasksToDoEl.appendChild(listItemEl);
-}
+  };
 
-// Add a click event listener for addTaskButton to run the createTaskHandler function 
-addTaskButton.addEventListener("click", createTaskHandler);
+// Add a click event listener for formEl to run the createTaskHandler function 
+formEl.addEventListener("submit", createTaskHandler);
