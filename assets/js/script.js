60e551688bc0e6e98d7e07f317060710424c4319
Alexandra Holden <alexandraholden@Alexandras-MacBook-Air.local>
2022-01-10 18:36:51 -0500

    established initial html for new form submission feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,11 @@ var tasksToDoEl = document.querySelector("#tasksToDo");
 
 // Create a function that appends child li elements to the parent ul elemnt
 var createTaskHandler = function () {
+    // Initiate first callback function
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
+    // Initiate second callback function
     tasksToDoEl.appendChild(listItemEl);
 }
 
