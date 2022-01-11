d4c3458a107fbe68c0f3b9005f4ad11c8aef62be
Alexandra Holden <alexandraholden@Alexandras-MacBook-Air.local>
2022-01-10 21:41:12 -0500

    debugged createTaskEl function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var formEl = document.querySelector("#task-form");
-var tasksToDoEl = document.querySelector("#tasks-to-do");
+var tasksToDoEl = document.querySelector("#tasksToDo");
 
 var taskFormHandler = function(event) {
   event.preventDefault();
